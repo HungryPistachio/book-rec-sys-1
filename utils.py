3b51b7b83e1859d04bae6fe074ada4a6822392d9
HungryPistachio <119539479+HungryPistachio@users.noreply.github.com>
2024-11-12 15:53:34 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,5 @@
-# utils.py
-
-import pandas as pd
-
 def pad_missing_columns(input_data, feature_names):
-    # Add missing columns to input_data to match feature_names, setting them to zero if missing
-    for feature in feature_names:
-        if feature not in input_data.columns:
-            input_data[feature] = 0
-    return input_data[feature_names]
+    for col in feature_names:
+        if col not in input_data.columns:
+            input_data[col] = 0
+    return input_data[feature_names]  # Ensure the column order matches feature_names
