1ef87c5372c2c39ff0686238387fb04b2f588fb3
HungryPistachio <119539479+HungryPistachio@users.noreply.github.com>
2024-11-13 19:34:50 +0000

    Update anchor_explanations.py

--- xai/anchor_explanations.py
+++ xai/anchor_explanations.py
@@ -72,6 +72,8 @@ def get_anchor_explanation_for_recommendation(recommendation, original_vector):
         }
 
 def get_anchor_explanation(recommendations, original_feature_names):
+    logging.info(f"get_anchor_explanation received recommendations data: {json.dumps(recommendations, indent=2)}")
+    logging.info(f"get_anchor_explanation received original_feature_names data: {json.dumps(original_feature_names, indent=2)}")
     explanations = []
     # Prepare the vectorizer and original vector once
     original_vector = prepare_vectorizer_and_original_vector(original_feature_names)
